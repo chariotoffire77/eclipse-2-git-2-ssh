33284a039ba379da8fed9774a9c6691d295d4321
chariotoffire77 <kekeina920@gmail.com>
2019-07-01 04:58:20 -0400

    Initial commit of eclipse 2 git demo via ssh

---
+++ Testing-from-Eclipse-2-Git2/src/com/maestro/alala/HelloMaestro.java
@@ -0,0 +1,12 @@
+package com.maestro.alala;
+
+public class HelloMaestro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello this is a test, greeting from HelloMaestro 2 via ssh......");
+
+	}
+
+}
