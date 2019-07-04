8fa4c7bdc036d6655cfc8d30a17cec1ae766e288
chariotoffire77 <kekeina920@gmail.com>
2019-07-04 09:47:46 -0400

    Finally we succeed in pushing from eclipse using git and ssh

--- Testing-from-Eclipse-2-Git2/src/com/maestro/alala/HelloMaestro.java
+++ Testing-from-Eclipse-2-Git2/src/com/maestro/alala/HelloMaestro.java
@@ -6,6 +6,8 @@ public class HelloMaestro {
 		// TODO Auto-generated method stub
 		
 		System.out.println("Hello this is a test, greeting from HelloMaestro 2 via ssh......");
+		
+		System.out.println("Hello this is a test, greeting from HelloMaestro 3 via ssh, happy 4th of July......");
 
 	}
 
